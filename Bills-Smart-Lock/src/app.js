3d4a62660689e7dfc191b8b1e186ecba45d8a91d
Bill Reilly <bill@rorasystems.com>
2019-08-08 14:48:17 -0400

    I added the Account Linking

--- Bills-Smart-Lock/src/app.js
+++ Bills-Smart-Lock/src/app.js
@@ -26,9 +26,33 @@ app.use(
 
 app.setHandler({
     LAUNCH() {
-        this.$speech.addText ('Welcome to Rora.  Would you like to lock your door? Check the status of your door? or unlock your door?')
-        this.$reprompt.addText ('Please say, lock my door, what is the status of my door, or unlock my door to control your door lock.')
-        return this.ask(this.$speech, this.$reprompt);
+        //Check if customer is signed in
+        if (!this.$request.getAccessToken()){ //Customer NOT signed in
+            this.showAccountLinkingCard();
+            if (this.isAlexaSkill()) {
+                this.$alexaSkill.tell("Please open the Alexa App to Link your Smart Lock account.");
+            }
+        } else{
+            //customer signed in
+            this.$speech.addText ('Welcome to Rora.  Would you like to lock your door? Check the status of your door? or unlock your door?')
+            this.$reprompt.addText ('Please say, lock my door, what is the status of my door, or unlock my door to control your door lock.')
+            this.ask(this.$speech, this.$reprompt);
+        }
+
+    },
+
+    ON_SIGN_IN() {
+        if (this.$googleAction.getSignInStatus() === 'CANCELLED') {
+            // User did not link their account
+            this.showAccountLinkingCard();
+        } else if (this.$googleAction.getSignInStatus() === 'OK') {
+            // User linked their account
+            this.toIntent("LAUNCH");
+            this.tell(this.$speech);
+        } else if (this.$googleAction.getSignInStatus() === 'ERROR') {
+            // There was an error
+            this.showAccountLinkingCard();
+        }
     },
 
     LockIntent() {
